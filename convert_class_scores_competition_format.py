87fe8712b717dc6cdebf5cfff0d43c763ba759a0
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2021-10-21 16:27:50 +0500

    results

---
+++ convert_class_scores_competition_format.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 21 16:13:51 2021
+
+@author: ali
+"""
+
+
+import numpy as np
+import json
+
+
+base_dir = './tutorial_exps/'
+
+f = open(base_dir+'output_results.json')
+results_dict = json.load(f)
+
+formatted_results = []
+for i in results_dict['class_scores']:
+    print(i)
+    formatted_results.append(str(i[0])+" "+ str(i[1])+" "+str(i[2]))
+ 
+# Closing file
+f.close()
+
+formatted_results = np.asarray(formatted_results)
+np.savetxt(fname=base_dir+'output_results_formatted.txt',X=formatted_results, fmt='%s')
